f149bfcdb9a7c977c29934de99d1dfb06d243450
programmer0325 <programmmer0325@gmail.com>
2024-01-13 13:46:43 +0500

    movie project

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -43,7 +43,7 @@ class _HomePageState extends State<HomePage> {
                 "Trending Movies",
                 style: TextStyle(
                   color: Theme.of(context).colorScheme.primary,
-                  fontSize: 25,
+                  fontSize: 30,
                   fontWeight: FontWeight.bold,
                 ),
               ),
